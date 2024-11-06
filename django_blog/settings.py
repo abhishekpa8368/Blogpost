d9a1f92eeb5e796761a1b27c16fae46a82c6ae3c
Abhishek Pal <118683730+abhishekpa8368@users.noreply.github.com>
2024-11-06 11:38:48 +0530

    Update settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i!tpa18*=_t0lozqy&7fb9b5yg-%54_!lfspw&ye%vfd!0=fgx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
